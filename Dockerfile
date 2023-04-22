f50d9548ecee6377b7a189b5f87ab797aaceb84c
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2023-04-22 17:47:46 +0200

    Trying to run headless using jenkins and docker

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
 RUN chmod +x test_runner.sh
-CMD ["pytest", "./tests/test_api.py"]
+CMD ["bash", "./test_runner.sh"]
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,25 @@
+import os.path
 import pytest
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 
 @pytest.fixture
-def driver():
+def setup_chrome_options():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-popup-blocking")
     options.add_argument('--disable-save-password-bubble')
+    if os.path.exists('/.dockerenv'):
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        
+
+@pytest.fixture
+def driver(setup_options):
+    options = setup_options
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
+  
